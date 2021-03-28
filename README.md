0748a6f1c5609273b0e116d0011ce2d7c4057672
Anatoli Makarevich <makaroni4@gmail.com>
2021-03-28 18:19:47 +0100

    Add custom font

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 ## TODO
 
-- [] When proceed allow 30min browsing without screen wall
-- [] QA on all popular time killing websites
 - [] Add scoped SCSS support
 - [] Design system
+- [] QA on all popular time killing websites
+- [] When proceed allow 30min browsing without screen wall
 - [] Move chrome logic into a module
 - [] Extension popup final design
 - [] Extension options page
   - [] Allow deletion of websites
+  - [] Allow editing of websites
   - [] Add throttled "Saved" popup
 - [] Extension popup design
 - [] Implement simple pattern matching with * for any symbol, like google.com*NBA
 - [] Fix ES lint
 - [] Store counts only for the last 30 days
+- [] Make screenshots
+- [] Create Chrome page
+- [] Create Firefox page
+- [] Create README file
--- src/components/ScreenWall.vue
+++ src/components/ScreenWall.vue
@@ -61,15 +61,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-@font-face {
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 400;
-  src: url("chrome-extension://__MSG_@@extension_id__/fonts/Roboto-Regular.ttf");
-}
-
+<style lang="scss">
 .oh-really-popup {
   position: fixed;
   top: 0;
@@ -82,19 +74,21 @@ export default {
   border-radius: 0 0 8px 0;
 
   background-color: #FFB088;
-}
 
-.oh-really-popup__icon {
-  width: 32px;
-  height: 32px;
-  display: block;
+  font-family: "Muli", sans-serif;
 
-  background-image: url("chrome-extension://__MSG_@@extension_id__/images/edit-regular.svg") !important;
-  background-repeat: no-repeat;
-  background-position: center;
+  &__icon {
+    width: 32px;
+    height: 32px;
+    display: block;
 
-  color: #FFF;
+    background-image: url("chrome-extension://__MSG_@@extension_id__/images/edit-regular.svg") !important;
+    background-repeat: no-repeat;
+    background-position: center;
 
-  cursor: pointer;
+    color: #FFF;
+
+    cursor: pointer;
+  }
 }
 </style>
